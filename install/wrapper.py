a32eeff0aedbf1e1973b4dded9b22d8e6f3d2b88
jurjen93 <jurjendejong93@gmail.com>
2024-11-20 16:42:07 +0100

    refactor

--- install/wrapper.py
+++ install/wrapper.py
@@ -6,7 +6,7 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, parent_dir)  # Insert at the start to prioritize this path
 
 # Import the main function from facetselfcal.py in the parent directory
-from ..facetselfcal import main as facetselfcal_main
+from facetselfcal import main as facetselfcal_main
 
 def main():
     # Run the main function from facetselfcal
