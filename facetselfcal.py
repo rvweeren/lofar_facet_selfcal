f60e858771ff22d7c09523417785dbdea2203140
jurjen93 <jurjendejong93@gmail.com>
2024-11-20 18:30:55 +0100

    imports

--- facetselfcal.py
+++ facetselfcal.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# add standard tests to lofar_facet_selfcal to ensure clean development
+# continue splitting functions in facetselfcal in separate modules
 # auto update channels out and fitspectralpol for high dynamic range
 # h5_merger.merge_h5(h5_out=outparmdb,h5_tables=parmdb,add_directions=sourcedir_removed.tolist(),propagate_weights=False) Needs to be propagate_weights to be fully correct, this is a h5_merger issue
 # check that MODEL_DATA_DD etc XY,YX are set to zero/or clean if wsclean predicts are used for Stokes I/dual
@@ -73,8 +75,26 @@ import scipy.stats
 import time
 
 # modules
-try:
-    # Absolute import (works when running the script as part of the package)
+if __package__:
+    # Relative import (works when running as part of the package)
+    from .submods.source_selection.selfcal_selection import main as quality_check
+    from .submods.split_irregular_timeaxis import regularize_ms, split_ms
+    from .submods.h5_helpers.reset_structure import fix_h5
+    from .submods.h5_merger import merge_h5
+    from .submods.h5_helpers.split_h5 import split_multidir
+    from .submods.h5_helpers.overwrite_table import copy_over_source_direction_h5
+    from .submods.h5_helpers.modify_amplitude import (flag_bad_amps, get_median_amp, normamplitudes,
+                                                     normslope_withmatrix,
+                                                     normamplitudes_withmatrix, flaglowamps, flaghighamps,
+                                                     flaghighamps_fulljones)
+    from .submods.h5_helpers.modify_rotation import rotationmeasure_to_phase, fix_weights_rotationh5, fix_rotationreference
+    from .submods.h5_helpers.modify_tec import fix_tecreference
+    from .submods.h5_helpers.nan_values import remove_nans, removenans_fulljones
+    from .submods.h5_helpers.update_sources import update_sourcedirname_h5_dde, update_sourcedir_h5_dde
+    from .submods.h5_helpers.general import make_utf8
+    from .arguments import option_parser
+else:
+    # Absolute import (works when running the script directly)
     from submods.source_selection.selfcal_selection import main as quality_check
     from submods.split_irregular_timeaxis import regularize_ms, split_ms
     from submods.h5_helpers.reset_structure import fix_h5
@@ -92,25 +112,7 @@ try:
     from submods.h5_helpers.update_sources import update_sourcedirname_h5_dde, update_sourcedir_h5_dde
     from submods.h5_helpers.general import make_utf8
     from arguments import option_parser
-except ImportError:
-    # Relative import (works when running the script directly)
-    from .submods.source_selection.selfcal_selection import main as quality_check
-    from .submods.split_irregular_timeaxis import regularize_ms, split_ms
-    from .submods.h5_helpers.reset_structure import fix_h5
-    from .submods.h5_merger import merge_h5
-    from .submods.h5_helpers.split_h5 import split_multidir
-    from .submods.h5_helpers.overwrite_table import copy_over_source_direction_h5
-    from .submods.h5_helpers.modify_amplitude import (flag_bad_amps, get_median_amp, normamplitudes,
-                                                      normslope_withmatrix,
-                                                      normamplitudes_withmatrix, flaglowamps, flaghighamps,
-                                                      flaghighamps_fulljones)
-    from .submods.h5_helpers.modify_rotation import rotationmeasure_to_phase, fix_weights_rotationh5, \
-        fix_rotationreference
-    from .submods.h5_helpers.modify_tec import fix_tecreference
-    from .submods.h5_helpers.nan_values import remove_nans, removenans_fulljones
-    from .submods.h5_helpers.update_sources import update_sourcedirname_h5_dde, update_sourcedir_h5_dde
-    from .submods.h5_helpers.general import make_utf8
-    from .arguments import option_parser
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='selfcal.log',
