d573714a9fa812f3d63b8c3f5ae0fcbb72c1a19c
jurjen93 <jurjendejong93@gmail.com>
2025-04-02 22:08:01 +0200

    multidir merge

---
+++ submods/h5_helpers/multidir_h5.py
@@ -0,0 +1,44 @@
+import tables
+import numpy as np
+try:
+    from .slicing import get_slices
+except:
+    from slicing import get_slices
+
+
+def same_weights_multidir(h5: str = None):
+    """
+    Enforce same weights for all directions in multidir h5parm
+
+    Args:
+        h5: multidir-h5
+    """
+
+    with tables.open_file(h5, "r+") as H:
+        for soltab in H.root.sol000._v_groups.keys():
+            st = H.root.sol000._f_get_child(soltab)
+            weights = st.weight
+            axes = weights.attrs["AXES"].decode("utf8").split(',')
+            if 'dir' in axes:
+                H.root.sol000._f_get_child(soltab).weight[:] = np.prod(weights[:], axis=axes.index("dir"), keepdims=True)
+            else:
+                print(f"WARNING: no directions in solution table: {h5}/{soltab}")
+
+
+def is_multidir(h5: str = None):
+    """
+    Check if h5 has multiple directions
+
+    Args:
+        h5: h5parm
+
+    Returns: boolean
+
+    """
+
+    with tables.open_file(h5) as H:
+        for soltab in H.root.sol000._v_groups.keys():
+            dirs = H.root.sol000._f_get_child(soltab).dir[:]
+            if len(dirs) > 1:
+                return True
+    return False
