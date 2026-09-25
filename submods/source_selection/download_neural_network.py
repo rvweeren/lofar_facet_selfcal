15a354d32a75fe34e745cc2506bac7ff6c000097
jurjen93 <jurjendejong93@gmail.com>
2026-09-25 16:28:44 +0200

    update default ai model

--- submods/source_selection/download_neural_network.py
+++ submods/source_selection/download_neural_network.py
@@ -51,7 +51,7 @@ def main():
     parser = ArgumentParser("Download neural network")
     parser.add_argument('--cache_directory', help='Cache folder with model', default='.cache/cortexchange')
     parser.add_argument('--device', help='CPU or GPU', default='cpu')
-    parser.add_argument('--model', help='Model name', default='surf/dino_big_lora_tune_posclsreg_may_O2_aug_099')
+    parser.add_argument('--model', help='Model name', default='surf/dinov2_vitb14_lora_O2_aug_0984')
 
     args = parser.parse_args()
 
